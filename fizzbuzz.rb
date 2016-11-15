83628436226ccdea7eab3671186a5dd1bc029ffb
Daniel Seehausen <daniel.seehausen@gmail.com>
2016-11-15 03:43:04 +0000

    Done.

---
+++ fizzbuzz.rb
@@ -0,0 +1,14 @@
+
+
+def fizzbuzz(s)
+    i = s.to_i
+    if (i%3 ==0) && (i%5 == 0)
+        return "FizzBuzz"
+    elsif (i%3 == 0)
+        return "Fizz"
+    elsif (i%5 == 0)
+        return "Buzz"
+    else
+        return nil
+    end
+end
