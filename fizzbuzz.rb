1923042b324220a4189093651cae6c0f12531ef5
Mike Merin <mikemerinweather@gmail.com>
2017-02-20 06:42:43 +0000

    Done.

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -8,3 +8,32 @@ def fizzbuzz(n)
   s == "" ? nil : s
 
 end
+
+
+=begin
+
+# alt methods:
+
+def fizzbuzz(n)
+
+  return "Fizz" if n % 3 == 0 && n % 5 != 0
+  return "Buzz" if n % 3 != 0 && n % 5 == 0
+  return "FizzBuzz" if n % 3 == 0 && n % 5 == 0
+  return nil
+  
+end
+
+def fizzbuzz(n)
+
+  if n % 3 == 0 && n % 5 == 0
+    "FizzBuzz"
+  elsif n % 3 == 0
+    "Fizz"
+  elsif n % 5 == 0
+    "Buzz"
+  end
+    
+end
+
+
+=end
