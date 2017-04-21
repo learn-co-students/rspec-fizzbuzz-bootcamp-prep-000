057496c3a783418b59f9d26cecab86a05c8024ce
Steven Ossorio <codebusiness2014@gmail.com>
2017-04-21 15:52:12 +0000

    Done.

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,10 +1,10 @@
 def fizzbuzz(n)
-  if n % 3 == 0
-    return "Fizz"
+  if n % 3 == 0 && n % 5 == 0
+    return "FizzBuzz"
   elsif n % 5 == 0
     return "Buzz"
-  elsif n % 15 == 0
-    return "FizzBuzz"
+  elsif n % 3 == 0
+    return "Fizz"
   else
     return nil
   end
