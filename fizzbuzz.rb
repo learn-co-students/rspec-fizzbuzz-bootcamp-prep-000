36c2f91c08962c5b9193f6f889ddea1458e5de4b
Gabriel DeFazio <aloveletter@gmail.com>
2017-05-31 14:19:05 +0000

    Done.

---
+++ fizzbuzz.rb
@@ -0,0 +1,20 @@
+def fizzbuzz(int)
+ if int % 3 ==0 && int % 5 != 0
+    "Fizz"
+ elsif int % 5 == 0 && int % 3 != 0
+    "Buzz"
+elsif int % 15 == 0
+    "FizzBuzz"
+ end
+end
+
+
+def fizzbuzz(int)
+  if int % 3 ==0 && int % 5 == 0
+    "FizzBuzz"
+  elsif int % 5 == 0
+    "Buzz"
+  elsif int % 3 == 0
+    "Fizz"
+  end
+end
