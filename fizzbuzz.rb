ccfb1817861d6d843a109b184c6c4ace57ba6fae
Jessica Holliday <hazenbrassy@gmail.com>
2017-06-25 05:25:51 +0000

    Done.

---
+++ fizzbuzz.rb
@@ -0,0 +1,9 @@
+def fizzbuzz(x)
+  if (x % 5 == 0 && x % 3 == 0) # if the number int is divisible by 3
+    "FizzBuzz" # Go fizz
+  elsif x % 3 == 0 # if the number int is divisible by 3
+    "Fizz" # Go fizz
+  elsif x % 5 == 0 # if the number int is divisible by 3
+  "Buzz" # Go fizz
+  end
+end
