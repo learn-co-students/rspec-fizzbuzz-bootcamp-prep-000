366abc89070d7fa0bf2ba383cde2025ba30d6921
Melissa Amaya <amaya.melissa@gmail.com>
2017-07-10 11:48:13 +0000

    Done.

---
+++ fizzbuzz.rb
@@ -0,0 +1,11 @@
+def fizzbuzz(int)
+  if int % 3 == 0
+     "Fizz"
+    #  if int % 3 == 0 && int % 5 == 0
+    #      "Fizzbuzz"
+    #  else if int % 3 == 0
+    #     "Fizz"
+    #  else if int % 5 == 0
+    #     "Buzz"
+    #  else  nil
+end
