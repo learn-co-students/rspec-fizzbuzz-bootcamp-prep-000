571f1e90931aded5ded853e705f7d814c515cf46
Melissa Amaya <amaya.melissa@gmail.com>
2017-07-10 16:29:54 +0000

    Done.

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,11 +1,11 @@
 def fizzbuzz(int)
-  if int % 3 == 0
-     "Fizz"
-    #  if int % 3 == 0 && int % 5 == 0
-    #      "Fizzbuzz"
-    #  else if int % 3 == 0
-    #     "Fizz"
-    #  else if int % 5 == 0
-    #     "Buzz"
-    #  else  nil
+
+  if int % 3 == 0 && int % 5 == 0
+          "FizzBuzz"
+       elsif int % 3 == 0 # if the number int is divisible by 3
+         "Fizz"
+       elsif int % 5 == 0
+           "Buzz"
+     else  nil
+     end
 end
