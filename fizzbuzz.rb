53833e15c776f958f61fb4ff6fea553a1e6a31d0
Kariny Neves Parreira de Vasconcelos Vasconcelos <karinyparreira@hotmail.com>
2017-07-21 17:03:24 +0000

    Done.

---
+++ fizzbuzz.rb
@@ -0,0 +1,13 @@
+def fizzbuzz(int)
+  if ( ((int % 3) == 0) && ((int % 5) == 0) )
+    "FizzBuzz"
+  else
+    if ((int % 3) == 0)
+      "Fizz"
+    else
+      if ((int % 5) == 0)
+        "Buzz"
+      end
+    end
+  end
+end
