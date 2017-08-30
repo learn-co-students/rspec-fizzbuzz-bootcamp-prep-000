ec80816068650a6e36575f75e501f4be908c7084
LG-STEM <gatesinfo@gmail.com>
2017-08-30 11:56:28 +0000

    Done.

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,7 +1,7 @@
 def fizzbuzz(int)
   if ((int % 3 == 0) && (int % 5 == 0))
     'FizzBuzz'
-  elsif ((int % 3 == 0) && (int % 5 != 0))
+  elsif int % 3 == 0 && int % 5 != 0
     'Fizz'
   elsif int % 5 == 0 && int % 3 != 0
     'Buzz'
