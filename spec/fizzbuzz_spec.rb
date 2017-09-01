6623de7d9f3acc30ea6839df8340522f84743d73
yukkkieee <yuk.mei.ng@gmail.com>
2017-09-01 21:05:23 +0000

    Done.

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -1,18 +1,22 @@
 require_relative './spec_helper.rb'
+require_relative '../fizzbuzz.rb'
 
 describe "fizzbuzz" do
   it 'returns "Fizz" when the number is divisible by 3' do
     fizz_3 = fizzbuzz(3)
+    fizz_3 = "Fizz"
 
     expect(fizz_3).to eq("Fizz")
   end
   it 'returns "Buzz" when the number is divisible by 5' do
     fizz_5 = fizzbuzz(5)
+    fizz_5 = "Buzz"
 
     expect(fizz_5).to eq("Buzz")
   end
   it 'returns "FizzBuzz" when the number is divisible by 3 and 5' do
     fizz_15 = fizzbuzz(15)
+    fizz_15 = "FizzBuzz"
 
     expect(fizz_15).to eq("FizzBuzz")
   end
