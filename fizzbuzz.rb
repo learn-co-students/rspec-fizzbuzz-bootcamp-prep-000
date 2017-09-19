008ca799b181a3a3398b4d4264f134b9313a9e12
Julie <jsingh@alumni.iu.edu>
2017-09-18 22:30:48 -0700

    Add files via upload

---
+++ fizzbuzz.rb
@@ -0,0 +1,12 @@
+def fizzbuzz(x)
+if ((x % 5 == 0) && (x % 3 == 0))
+    return "FizzBuzz"
+  elsif (x % 3 == 0)
+      return "Fizz"
+  elsif (x % 5 == 0)
+      return "Buzz"
+  else
+      return nil
+  end
+
+end
