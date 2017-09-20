ce03ddb47863c8ceb3bd9647bfcea13958bbbfe5
Moshe Maizels <moshemaizels@gmail.com>
2017-09-20 02:05:54 +0000

    Done.

---
+++ fizzbuzz.rb
@@ -0,0 +1,18 @@
+def fizzbuzz(int)
+  if int % 3 == 0 && int % 5 == 0
+    "FizzBuzz"
+  elsif int % 3 == 0
+    "Fizz"
+  elsif int % 5 == 0
+    "Buzz"
+  end
+#*if int % 3 == 0 && int % 5 == 0
+#    puts "FizzBuzz"
+#elsif int % 3 == 0
+#    puts "Fizz"
+#elsif int % 5 == 0
+#  puts "Buzz"
+#else
+  #return nil
+#end
+end
