a1d8eafce3d3554d4f9867f3eb6f16643b9044ee
Ion Gorincioi <gorincioii@yahoo.com>
2017-09-22 17:24:04 +0000

    Done.

---
+++ fizzbuzz.rb
@@ -0,0 +1,13 @@
+def fizzbuzz(n)
+  if ((n % 3 == 0) && (n % 5 == 0))
+    "FizzBuzz"
+  elsif ((n % 3 != 0) && (n % 5 != 0))
+    nil  
+  elsif n % 3 == 0
+    "Fizz"
+  elsif n % 5 == 0
+    "Buzz"
+  else
+    nil
+  end
+end
