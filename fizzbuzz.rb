b60b6beabe76b6f0f482e40c062139506f3f8df5
Rebecca Hong <ms.rebeccahong@gmail.com>
2017-10-04 08:57:16 +0000

    Done.

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,11 +1,9 @@
 def fizzbuzz(x)
-  if x % 3 == 0 && x % 5 != 0
-    "Fizz"
-  elsif x % 5 == 0 && x % 3 != 0
-    "Buzz"
-  elsif x % 3 == 0 && x % 5 == 0
+  if x % 3 == 0 && x % 5 == 0
     "FizzBuzz"
-  else
-    nil
+  elsif x % 3 == 0
+    "Fizz"
+  elsif x % 5 == 0
+    "Buzz"
   end
 end
