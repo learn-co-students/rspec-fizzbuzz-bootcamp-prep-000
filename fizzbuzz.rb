1ea29f1908f1ad0f8d12e7fa622bc420336732f3
Man Hong Lee <manhong323@gmail.com>
2017-10-28 23:43:59 +0000

    Done.

---
+++ fizzbuzz.rb
@@ -0,0 +1,16 @@
+def fizzbuzz(int)
+  if int % 15 == 0
+    return "FizzBuzz"
+  end
+
+  if int % 3 == 0
+    return "Fizz"
+  end
+
+  if int % 5 == 0
+    return "Buzz"
+  end
+end
+
+
+puts fizzbuzz(3)
