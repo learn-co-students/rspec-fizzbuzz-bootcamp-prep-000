a1b3a605fbb3160f9f84d63562de44b8ca2ddf9f
Nicole Campbell <nscampbell303@gmail.com>
2017-11-12 21:38:18 +0000

    Done.

---
+++ fizzbuzz.rb
@@ -0,0 +1,10 @@
+require_relative './spec/fizzbuzz_spec.rb'
+
+if fizzbuzz == fizz_3
+  puts "Fizz"
+elsif fizzbuzz == fizz_5
+  puts "Buzz"
+elsif fizzbuzz == fizz_15
+  puts "FizzBuzz"
+else
+  puts "nil"
