c4fe4655356ce928a3b86d73cb47195eb4b2db03
Nicole Campbell <nscampbell303@gmail.com>
2017-11-17 22:47:21 +0000

    Done.

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,10 +1,11 @@
-require_relative './spec/fizzbuzz_spec.rb'
-
-if fizzbuzz == fizz_3
-  puts "Fizz"
-elsif fizzbuzz == fizz_5
-  puts "Buzz"
-elsif fizzbuzz == fizz_15
-  puts "FizzBuzz"
-else
-  puts "nil"
+def fizzbuzz(init)
+  if init % 15 == 0
+    "FizzBuzz"
+  elsif init % 5 == 0
+    "Buzz"
+  elsif init % 3 == 0
+    "Fizz"
+  else
+    nil
+  end
+end
