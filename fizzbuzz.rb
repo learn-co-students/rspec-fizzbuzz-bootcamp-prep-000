0c91591d1ebeb85fdad72b22c26a7dd109206b0e
Jacob Denbeaux <jdenbeaux19@wooster.edu>
2017-12-11 23:40:54 +0100

    Done.

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,12 +1,13 @@
 def fizzbuzz n
-  #if n % 3 == 0
-  #  "Fizz"
-  #end
-  if n % 15 == 0
-    "FizzBuzz"
-  elsif n % 3 == 0
-    "Fizz"
-  elsif n % 5 == 0
-    "Buzz"
+  out = ""
+  if n % 3 == 0
+    out += "Fizz"
   end
+  if n % 5 == 0
+    out += "Buzz"
+  end
+  if out == ""
+    out = nil
+  end
+  out
 end
