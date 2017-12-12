ac4ed0e447fa673ce889a69e430a4cc93fe05ff8
Anthony Catalfo <anthonycatalfo@gmail.com>
2017-12-12 04:22:23 +0000

    Done.

---
+++ fizzbuzz.rb
@@ -0,0 +1,19 @@
+def fizzbuzz(int)
+
+num=int
+
+      while int > 0
+        case int
+      when 1,2,4,7,8,11,13,14
+         return nil
+      when 3,6,9,12
+         return 'Fizz'
+      when 5,10
+      return 'Buzz'
+      when 15
+      return 'FizzBuzz'
+      else
+         int=int-15
+      end
+    end
+end
