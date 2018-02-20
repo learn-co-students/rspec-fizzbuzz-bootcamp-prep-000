fa9b9b4c50378d94f95592dbf7388eb5cd69c9fb
Sara Ginsburg <sara.ginsburg.ny@gmail.com>
2018-02-20 22:06:49 +0000

    Done.

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -2,17 +2,13 @@
 
 def fizzbuzz(num)
   if (num % 3 == 0 &&  num % 5 == 0)
-    fizz_15 = "FizzBuzz"
-    return fizz_15
-  else
-    if (num % 5 == 0)
-      fizz_5 = "Buzz"
-      return  fizz_5
-    else
-      if (num % 3 == 0)
-        fizz_3 = "Fizz"
-        return fizz_3
-      end
-    end
+    "FizzBuzz"
+  elsif (num % 5 == 0)
+       "Buzz"
+
+  elsif (num % 3 == 0)
+       "Fizz"
+
   end
+  
 end
