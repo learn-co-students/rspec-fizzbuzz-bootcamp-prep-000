ee0fa078322ed2b6e428d7eeaaab10d64d76c25b
SDS 800 <jeremyhyman@gmail.com>
2018-03-01 20:01:18 +0000

    Done.

---
+++ fizzbuzz.rb
@@ -0,0 +1,12 @@
+def fizzbuzz(int)
+  if int % 5 == 0 && int % 3 == 0 # if the number int is divisible by 3
+    "FizzBuzz" # Go fizz
+
+    elsif int % 5 == 0
+      "Buzz"
+    elsif int % 3 == 0
+      "Fizz"
+
+
+  end
+end
