c8339b2b447d99a9ea32c9175d9f8cbe48d64d6f
ochoa.kayla@yahoo.com <ochoa.kayla@yahoo.com>
2018-04-23 22:36:21 +0000

    Done.

---
+++ fizzbuzz.rb
@@ -0,0 +1,13 @@
+require 'pry'
+
+
+def fizzbuzz(int)
+  if int % 3 == 0 && int % 5 == 0 
+    "FizzBuzz"
+  elsif int % 3 == 0 
+    "Fizz"
+  elsif int % 5 == 0 
+    "Buzz"
+    binding.pry
+  end
+end
