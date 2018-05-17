8a63355d024557d45c485e94592091dee513ddb2
jfburk <cineburk@gmail.com>
2018-05-17 23:23:58 +0000

    Done.

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,4 +1,18 @@
 def fizzbuzz(num)
-  
+  if num%3 == 0 && num%5 == 0
+    return "FizzBuzz"
+  elsif num%3 == 0
+    return "Fizz"
+  elsif num%5 == 0
+    return "Buzz"
+  else
+    return NIL
+  end
   
 end
+
+fizzbuzz(6)
+fizzbuzz(20)
+fizzbuzz(60)
+fizzbuzz(2)
+fizzbuzz(10)
