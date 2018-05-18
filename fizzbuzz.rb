363faf705897da02566581e4623d86802e67aee7
Gabriel Miranda <gabrielcm.br@gmail.com>
2018-05-18 14:19:43 +0000

    Done.

---
+++ fizzbuzz.rb
@@ -0,0 +1,13 @@
+def fizzbuzz(num)
+  result = ""
+  if (num%3 == 0)
+    result += "Fizz"
+  end
+  if (num%5 == 0)
+    result += "Buzz"
+  end
+  if (result == "")
+    return nil
+  end
+  return result
+end
