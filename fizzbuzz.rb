e2d990c073eecaaed5ee9d84fb96625a3a6344af
Dina Hafez <dinahafez.8@gmail.com>
2018-06-06 19:08:03 +0000

    Done.

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,11 +1,12 @@
 
 def fizzbuzz(input)
-  if input % 3 == 0
-    print "Fizz"
+  if input % 3 == 0 && input % 5 == 0 
+    "FizzBuzz"
+  elsif input % 3 == 0
+    "Fizz"
   elsif input % 5 == 0
-    print "Buzz"
-  elsif input % 3 && input % 5 == 0 
-    print "FizzBuzz"
-  else input % 4 == 0
+    "Buzz"
+  else 
     print "nil"
+  end
 end
