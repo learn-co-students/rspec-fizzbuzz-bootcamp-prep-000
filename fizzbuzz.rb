234d1317984f044d3ddce0c14b1b2aff683cb53e
Danny O'B <dannyk85.dob@gmail.com>
2018-06-13 18:29:49 +0000

    Done.

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,2 +1,18 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
+
+def fizzbuzz(number)
+  retVal = ""
+  if number%3 == 0 || number%5 ==0
+    if number%3 == 0 && number%5 == 0
+      retVal = "FizzBuzz"
+    elsif number%3 == 0
+      retVal = "Fizz"
+    elsif number%5 == 0
+      retVal = "Buzz"
+    end
+  else
+    retVal = NIL
+  end
+  return retVal
+end
