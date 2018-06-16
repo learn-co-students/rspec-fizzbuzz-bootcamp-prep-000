0af21861f56c7bffe8348aa3902c415ea9c6a87a
Steven Crouse <sdcrouse91@gmail.com>
2018-06-16 06:39:35 +0000

    Done.

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,2 +1,12 @@
-# Don't forget! This file needs to be 'required' in its spec file
-# See README.md for instructions on how to do this
+def fizzbuzz(int)
+  # This also works for negative numbers!
+  
+  if int % 15 == 0 # I could also say int % 3 == 0 && int % 5 == 0 
+    "FizzBuzz"
+  elsif int % 3 == 0 # I could put another if statement inside this (if int % 5 == 0)
+    "Fizz"
+  elsif int % 5 == 0 
+    "Buzz"
+  end
+  
+end
