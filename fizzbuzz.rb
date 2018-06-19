fcf9b9f85c649b5e2345c750349b7c14873215c7
Christabel Dapaa-Addo <cdapaaaddo@gmail.com>
2018-06-19 00:34:14 +0000

    Done.

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,2 +1,14 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
+def fizzbuzz(int)
+  if int % 3 == 0 && int % 5 == 0
+    num = "FizzBuzz"
+  elsif int % 3 == 0
+    num = "Fizz"
+  elsif int % 5 == 0
+    num = "Buzz"
+  else
+    num = nil
+  end
+  return num
+end
