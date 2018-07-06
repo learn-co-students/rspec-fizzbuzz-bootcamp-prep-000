3bf4561cfe9f283df73a46da4e439468b1fba416
Glen Simpson <gsimpson@airmaster.com.au>
2018-07-06 11:25:10 +0000

    Done.

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,2 +1,19 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
+
+
+def fizzbuzz(int)
+  fizz = int % 3
+  buzz = int % 5
+  
+  if(fizz == 0 && buzz == 0)
+    return "FizzBuzz"
+  elsif(fizz == 0)
+    return "Fizz"
+  elsif(buzz == 0)
+    return "Buzz"
+  else
+    return NIL
+  end
+
+end
