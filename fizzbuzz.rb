ef0f59b1d73d7b8eb88ad7cfaef1b127165c07f2
Jing Cao <jingcao.yale@gmail.com>
2018-07-11 21:48:11 +0000

    Done.

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,2 +1,18 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
+
+def fizzbuzz(num)
+
+  if num % 3 == 0 && num % 5 != 0
+    puts "Fizz"
+  end
+
+  if num % 3 != 0 && num % 5 == 0
+    puts "Buzz"
+  end
+
+  if num % 3 == 0 && num % 5 == 0
+    puts "FizzBuzz"
+  end
+
+end
