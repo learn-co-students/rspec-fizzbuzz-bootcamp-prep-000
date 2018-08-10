1f82f8a313dfe3499cac4a8038b8a34caeae8637
ktochow1 <ktochow1@binghamton.edu>
2018-08-10 02:31:40 +0000

    Done.

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,2 +1,16 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
+
+
+def fizzbuzz(int)
+  if int % 15 == 0
+    return "FizzBuzz"
+  elsif int % 5 == 0
+    return "Buzz"
+  elsif int % 3 == 0 # if the number int is divisible by 3
+      return "Fizz"
+  else int % 4 == 0
+    return nil
+
+  end
+end
