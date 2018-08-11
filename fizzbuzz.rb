d746163bc21b695a1fc05a1f2cc783a3481ae0d5
Shawn Kearney <shawnpk29@me.com>
2018-08-11 00:49:44 +0000

    Done.

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,2 +1,14 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
+def fizzbuzz(number)
+  case
+  when number % 3 == 0 && number % 5 == 0
+    'FizzBuzz'
+  when number % 5 == 0
+    'Buzz'
+  when number % 3 == 0
+    'Fizz'
+  else
+    nil
+  end
+end
