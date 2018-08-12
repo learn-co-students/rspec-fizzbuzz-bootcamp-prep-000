5d4142a1be0fe80fb88f23b5f28c4f4aa1c8c5c9
Ayaa <ayaa1siddig5@gmail.com>
2018-08-12 04:36:51 +0000

    Done.

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,2 +1,13 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
+def fizzbuzz(num)
+  if num % 15 == 0 
+    fizz_15="FizzBuzz"
+    elsif num % 3 == 0
+   fizz_3="Fizz"
+   elsif num % 5 == 0 
+   fizz_5= "Buzz"
+  else puts "nil"
+  end 
+end 
+ 
