00a434686b23ff4ab3f268c3529c3ad42943b3b6
Erin Penner <epenner0522@gmail.com>
2018-08-18 23:57:19 +0000

    Done.

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,2 +1,14 @@
-# Don't forget! This file needs to be 'required' in its spec file
-# See README.md for instructions on how to do this
+def fizzbuzz(number)
+  result = ""
+
+  if number % 3 == 0
+    result = result + "Fizz"
+  end
+  if number % 5 == 0
+    result = result + "Buzz"
+  end
+  if number % 3 != 0 && number % 5 != 0
+    result = nil
+  end
+  return result
+end
