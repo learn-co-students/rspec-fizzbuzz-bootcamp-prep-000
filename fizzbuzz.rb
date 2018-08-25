b9f215176dc87085b6fcd8389d0fe5ebe91d8d45
Casey Zimmer <czimmer@ramapo.edu>
2018-08-25 00:11:28 +0000

    Done.

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,2 +1,28 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
+
+def fizzbuzz(number)
+  
+  val = ""
+  divisible = false
+  
+
+  if(number % 3 == 0)
+  
+    val += "Fizz"
+    divisible = true
+  end
+  if(number % 5 == 0)
+  
+    val += "Buzz"
+    divisible = true
+  end
+  
+  if(!divisible)
+    
+      return nil
+  end
+  
+  
+    return val
+end
