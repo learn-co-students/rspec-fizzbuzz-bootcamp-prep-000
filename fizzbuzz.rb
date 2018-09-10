0e0dff9b0bcf1855e1601d841d6a642804748863
viqi zevic <vicky.tanzil@gmail.com>
2018-09-10 20:48:03 +0000

    Done.

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,2 +1,15 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
+require 'pry'
+
+def fizzbuzz(int)
+  if int % 3 == 0
+    if int % 5 == 0
+      "FizzBuzz"
+    else
+      "Fizz"
+    end
+  elsif int % 5 == 0
+    "Buzz"
+  end
+end
