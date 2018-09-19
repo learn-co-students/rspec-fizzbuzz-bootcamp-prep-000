9b032682ab82832f9dd828f1c3f8f96f1760f2a7
Jakob Baumgartner <jakob.baumgartner@hotmail.com>
2018-09-19 11:28:48 +0000

    Done.

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,2 +1,18 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
+
+def fizzbuzz (number)
+
+  if number%5 == 0 && number%3 == 0
+    return "FizzBuzz"
+  end
+
+  if number%3 == 0
+    return "Fizz"
+  end
+
+  if number%5 == 0
+    return "Buzz"
+  end
+
+end
