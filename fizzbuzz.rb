773d99ca66001b559f6a67d8de1a553aa41c0961
richard.supak Supak <richard.supak@icloud.com>
2019-01-30 03:04:06 +0000

    Done.

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,2 +1,8 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
+def fizzbuzz(num)
+  return 'FizzBuzz' if (num % 5).zero? && (num % 3).zero?
+  return 'Fizz' if (num % 3).zero?
+  return 'Buzz' if (num % 5).zero?
+  nil
+end
