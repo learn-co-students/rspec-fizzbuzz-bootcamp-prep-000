ae1957b41b0cacc43caf21053063d3723cd4c16a
Dwayne Munro <dwaynemunro1845@gmail.com>
2019-01-31 18:03:22 +0000

    Done.

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,9 +1,12 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
-require_relative './spec_helper.rb'
 
 def fizzbuzz(num)
-  if(num % 3 == 0)
+  if(num % 3 == 0 && num % 5 == 0)
+    return "FizzBuzz"
+  elsif (num % 5 == 0)
+    return "Buzz"
+  elsif (num % 3 == 0)
     return "Fizz"
   end
 end
