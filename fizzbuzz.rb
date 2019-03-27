39c578f820472a73ae653090e244e84babcfc00a
Segundo Bartra <segundobartra@gmail.com>
2019-03-27 23:42:40 +0000

    Done.

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,2 +1,7 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
+def fizzbuzz(number)
+  divByThree = ((number % 3) == 0)
+  divByFive = ((number % 5) == 0)
+  return (divByThree ? (divByFive ? "FizzBuzz" : "Fizz") : (divByFive ? "Buzz" : NIL))
+end
