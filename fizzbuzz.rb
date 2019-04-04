afae9169f40d7c42a171cc13841866f87aa6602d
Jonny Rodriguez <jojoroto@gmail.com>
2019-04-04 22:50:07 +0000

    Done.

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,2 +1,16 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
+def fizzbuzz(number)
+  result=""
+  if number%3 == 0
+    result="Fizz"
+  end
+  if number%5 == 0 
+    result+="Buzz"
+  end
+  if result!=""
+    return result
+  else
+    return NIL
+  end
+end
