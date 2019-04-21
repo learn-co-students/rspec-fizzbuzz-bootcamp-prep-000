b797a6e5091a5e4d3c931d3045e24695cf3e58e7
Alisher Zakir <a3zakir@gmail.com>
2019-04-21 16:38:09 +0000

    Done.

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,2 +1,10 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
+
+def fizzbuzz(n)
+  result = ""
+  result += "Fizz" if n % 3 == 0
+  result += "Buzz" if n % 5 == 0
+  return nil if result.length == 0
+  result
+end
