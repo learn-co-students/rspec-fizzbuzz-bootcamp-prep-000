b1d61fc189b978b17117fa8f3cc01e45dad83be4
mathematical-algorithm-3478 <jlboyer1@gmail.com>
2019-05-15 22:18:35 +0000

    Done.

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,2 +1,12 @@
-# Don't forget! This file needs to be 'required' in its spec file
-# See README.md for instructions on how to do this
+
+#!/usr/bin/env ruby
+
+def fizzbuzz(n)
+  if n % 3 == 0 && n % 5 == 0
+    "FizzBuzz"
+  elsif n % 3 == 0 && n % 5 != 0
+    "Fizz"
+  elsif n % 3 != 0 && n % 5 == 0
+    "Buzz"
+  end
+end
