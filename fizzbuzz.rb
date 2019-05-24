1baf070ecbd2acdc6046966b5cdaf7548baa9954
Alec Simpson <alecrsimpson@gmail.com>
2019-05-24 10:21:50 +0000

    Done.

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -2,11 +2,11 @@
 # See README.md for instructions on how to do this
 
 def fizzbuzz(int)
- if int % 3 == 0 and int % 5 == 0
-   return "FizzBuzz"
-   elsif int % 3 == 0
-   return "Fizz"
-   elsif int % 5 == 0
-   return "Buzz"
+  if int % 3 == 0 && int % 5 == 0
+    return "FizzBuzz"
+  elsif int % 3 == 0
+    return "Fizz"
+  elsif int % 5 == 0
+    return "Buzz"
   end
 end
