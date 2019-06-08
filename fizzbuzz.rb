d6fe9f69c7a1a55e06e9ed19d12efb7d02caf114
John Jordan Shelley <jshelley553@gmail.com>
2019-06-08 02:58:51 +0000

    Done.

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,2 +1,17 @@
-# Don't forget! This file needs to be 'required' in its spec file
-# See README.md for instructions on how to do this
+def fizzbuzz(num)
+  if num % 3 == 0 && !(num % 5 == 0)
+    fizz_3 = "Fizz"
+    p fizz_3
+  end
+
+  if num % 5 == 0 && !(num % 3 == 0)
+    fizz_5 = "Buzz"
+    p fizz_5
+  end
+  if num % 3 == 0 && num % 5 == 0
+    p "FizzBuzz"
+  end
+end
+fizzbuzz(3)
+fizzbuzz(5)
+fizzbuzz(15)
