4e013c38e3e57571a19261f9a1a9b14efef28b3b
Alex Stolzoff <alexstolznut@hotmail.com>
2019-09-08 23:29:16 +0000

    Done.

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,2 +1,12 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
+
+  if(FizzBuzz(3))
+    return "Fizz"
+  elsif(FizzBuzz(5))
+    return "Buzz"
+  elsif(FizzBuzz(5) &&  FizzBuzz(3))
+    return "FizzBuzz"
+  else
+    return nil
+  end
