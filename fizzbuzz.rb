85ebc1c493a85a295a7f7c4b797ba3d3ef813567
Kimberly Kohel-Hayes <kohelka@gmail.com>
2020-05-25 17:11:31 +0000

    Done.

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,2 +1,38 @@
-# Don't forget! This file needs to be 'required' in its spec file
-# See README.md for instructions on how to do this
+# Don't forget! This file needs to be 'required' in its spec file See README.md for instructions on how to do this
+#I need a method called #fizzbuzz that accepts one argument.
+#When I call that method and pass it a number divisible by 3, that method should return the string "Fizz".
+#When I call that method and pass it a number divisible by 5, that method should return the string "Buzz".
+#When I call that method and pass it a number divisible by 3 and 5, that method should return the string "FizzBuzz".
+#When I call that method and pass it a number not divisible by 3 or 5, that method should return nil.
+
+def fizzbuzz(number)
+  
+  if
+    
+    number / 3 == x
+    x.is_a?(Interger)=>true
+    
+    put "Fizz"
+  
+  elsif
+
+    number / 5 == x
+    x.is_a?(Interger)=>true
+    
+    put "Buzz"
+    
+  elsif
+
+    number / 15 == x
+    x.is_a?(Interger)=>true
+    
+    put "FizzBuzz"
+    
+   else
+
+    number / 15 == x
+    x.is_a?(Interger)=>false
+    
+    put "nil"
+   
+end
