8b0876f0e2ca90a108effea7e2e7f9e0e4cc9755
ronakvsoni <ronakvsoni@gmail.com>
2020-06-25 22:55:29 -0700

    Done.

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -2,19 +2,13 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
 def fizzbuzz(int)
-    if int % 3 == 0 # if the number int is divisible by 3
+  if int % 3 == 0 && int % 5 == 0
+    out = "FizzBuzz"
+  elsif int % 3 == 0 # if the number int is divisible by 3
       out = "Fizz" # Go fizz
-    end
-
-    if int % 5 == 0
-    puts out = "Buzz"
-    
-    end
-
-    if int % 3 == 0 && int % 5 == 0
-        out = "FizzBuzz"
-    end
-    return out
+  elsif int % 5 == 0
+     out = "Buzz"
+  end
 end
    
- puts fizzbuzz(15) # => You should see a return of "Fizz"
+ puts fizzbuzz(10) # => You should see a return of "Fizz"
