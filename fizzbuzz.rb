b01e31c973f43f449d8248f156a723414cf25d9c
Mimi C <mina123fire@yahoo.com>
2020-08-28 18:53:41 +0000

    Done.

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,2 +1,19 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
+def fizzbuzz(number)
+  if number % 3 == 0
+    three = true;
+    "Fizz"
+  end
+  if number % 5 == 0 
+    five = true;
+  end
+  
+  if three&&five
+    "FizzBuzz"
+  elsif three
+    "Fizz"
+  elsif five
+    "Buzz"
+  end
+end
