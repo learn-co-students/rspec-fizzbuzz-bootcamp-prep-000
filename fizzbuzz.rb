bc5bb6b35761508ce89fba5acdd26054b6f54dfd
Rishav Maskey <rishav.maskey@hotmail.com>
2021-04-19 06:17:28 +0000

    Done.

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,2 +1,11 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
+
+
+def fizzbuzz arg 
+  if arg % 3 != 0 && arg % 5 != 0
+    return nil
+  else
+    "#{arg % 3 == 0 ? "Fizz" : ""}#{arg % 5 == 0 ? "Buzz" : ""}"
+  end
+end
